758ebdbac745e27163354abca48a6b2d8cc5adea
Arafat Hossain <arafat15-12-603@diu.edu.bd>
2023-10-30 10:29:34 +0600

    Non blocking Asyncronus Way

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
 
-const TextIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking Syncronus Way
+// const TextIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(TextIn);
 
-const textOut = `This is what we know about the avocado : ${TextIn}.\nCreated On ${Date.now()} `;
-fs.writeFileSync("./txt/outPut.txt", textOut);
-console.log("File Written");
+// const textOut = `This is what we know about the avocado : ${TextIn}.\nCreated On ${Date.now()} `;
+// fs.writeFileSync("./txt/outPut.txt", textOut);
+// console.log("File Written");
+
+// Non Blocking Asyncronus Way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+  });
+});
+console.log("Asyncronous Way!!");
